814d3822ce7c8e37413c5d66d6798addd0d6dad7
rakib13x <rakib13x@gmail.com>
2023-11-04 06:08:14 +0600

    prisma updated again

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,7 +6,7 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey,Rakib here!</b> Discover my stories and creative ideas.
+        <b>Hello,Rakib here!</b> Discover my stories and creative ideas.
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
